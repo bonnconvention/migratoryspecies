ca186b108af9f1e43bd3151cd3b4c544b661e969
stefanbutura <stefan_butura@yahoo.com>
2018-06-26 16:12:06 +0300

    Revert "refs #3962 - migratoryspecies security updates."
    This reverts commit da3e5ce9ea767a5da291a71e6c90dbdd791e9eda.

--- drush/aliases/aliases.drushrc.php
+++ drush/aliases/aliases.drushrc.php
@@ -3,17 +3,12 @@
 $aliases['prod'] = array(
   'uri' => 'migratoryspecies.org',
   'db-allows-remote' => TRUE,
-  'remote-host' => 'www.unep-aewa.org',
+  'remote-host' => 'cms.int',
   'remote-user' => 'php',
   'root' => '/var/local/migratoryspecies/docroot',
   'path-aliases' => array(
     '%files' => 'sites/default/files',
-  ),
-  'command-specific' => array(
-    'sql-sync' => array(
-      'simulate' => '1',
-    ),
-  ),
+  )
 );
 
 // Add your local aliases.
